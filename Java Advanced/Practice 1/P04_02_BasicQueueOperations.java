1f74920c6758dcd2120c8058a410a55f5313f5a3
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-04-08 15:10:40 +0300

    Add files via upload

---
+++ Java Advanced/Practice 1/P04_02_BasicQueueOperations.java
@@ -0,0 +1,32 @@
+package JavaAdvanced.z_Practice.StacksQueuesExercise;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+public class P04_02_BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        int x = scanner.nextInt();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 1; i <= n; i++) {
+            queue.offer(scanner.nextInt());
+        }
+
+        for (int i = 1; i <= s; i++) {
+            queue.poll();
+        }
+
+        if (queue.isEmpty()) {
+            System.out.println(0);
+        } else {
+            if (queue.contains(x)) {
+                System.out.println("true");
+            } else {
+                System.out.println(Collections.min(queue));
+            }
+        }
+    }
+}
