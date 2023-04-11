dec47c7f535aa9d9f0a860422477ae1c1333a3d4
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-04-11 16:26:20 +0300

    Add files via upload

---
+++ Java Advanced/Practice 1/P06_02_PrintDiagonals.java
@@ -0,0 +1,29 @@
+package JavaAdvanced.z_Practice.MultidimensionalArraysLab;
+import java.util.Arrays;
+import java.util.Scanner;
+public class P06_02_PrintDiagonals {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[size][size];
+
+        for (int row = 0; row < matrix.length; row++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            matrix[row] = arr;
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (row == col) {
+                    System.out.print(matrix[row][col] + " ");
+                }
+            }
+        }
+        System.out.println();
+
+        for (int row = matrix.length - 1, col = 0; col < matrix.length; row--, col++) {
+            System.out.print(matrix[row][col] + " ");
+        }
+    }
+}
