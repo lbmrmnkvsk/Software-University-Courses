d468ff937708d249e8a09b3389297865ba64a240
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-04-16 13:24:48 +0300

    Add files via upload

---
+++ Java Advanced/Practice 1/P06_01_FixEmails.java
@@ -0,0 +1,27 @@
+package JavaAdvanced.z_Practice.SetsMapsExercise;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+public class P06_01_FixEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> emails = new LinkedHashMap<>();
+        String command = scanner.nextLine();
+
+        while (!command.equals("stop")) {
+            String name = command;
+            String email = scanner.nextLine();
+            String emailLC = email.toLowerCase();
+            if (!emailLC.endsWith("us") && !emailLC.endsWith("uk") && !emailLC.endsWith("com")) {
+                emails.put(name, email);
+            }
+
+            command = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, String> entry : emails.entrySet()) {
+            System.out.printf("%s -> %s%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
