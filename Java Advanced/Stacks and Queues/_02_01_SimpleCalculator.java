91c79e84b03e76d466ff3191c69cc17502a2d519
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-05-13 10:12:28 +0300

    Add files via upload

---
+++ Java Advanced/Stacks and Queues/_02_01_SimpleCalculator.java
@@ -0,0 +1,34 @@
+package JavaAdvanced.z_Practice.StacksQueuesLab;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class P02_01_SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        List<String> expressionParts = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+        Collections.reverse(expressionParts);
+
+        for (String part : expressionParts) {
+            stack.push(part);
+        }
+
+        while (stack.size() > 1) {
+            int first = Integer.parseInt(stack.pop());
+            String op = stack.pop();
+            int second = Integer.parseInt(stack.pop());
+            int result = 0;
+
+            switch (op) {
+                case "+": result = first + second;
+                break;
+                case "-": result = first - second;
+                break;
+            }
+            stack.push(result + "");
+        }
+
+        System.out.println(stack.peek());
+    }
+}
