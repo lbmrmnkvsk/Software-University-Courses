1e8024d5e47a4bc7cb373f1c2c3b1cde897be036
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-05-16 16:02:53 +0300

    Add files via upload

---
+++ Java Advanced/Multidimensional Arrays/P01_01_FillTheMatrix.java
@@ -0,0 +1,55 @@
+package JavaAdvanced.z_Practice.MultimensionalArraysExercise;
+import java.util.Scanner;
+public class P01_01_FillTheMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(", ");
+        int size = Integer.parseInt(input[0]);
+        String pattern = input[1];
+        int[][] matrix = new int[size][size];
+
+        if (pattern.equals("A")) {
+            fillMatrixPatternA(matrix);
+        } else if (pattern.equals("B")) {
+            fillMatrixPatternB(matrix);
+        }
+        printMatrix(matrix);
+    }
+
+    private static void fillMatrixPatternA(int[][] matrix) {
+        int startNumber = 1;
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = 0; row < matrix.length; row++) {
+                matrix[row][col] = startNumber;
+                startNumber++;
+            }
+        }
+    }
+
+    private static void fillMatrixPatternB(int[][] matrix) {
+        int startNumber = 1;
+        for (int col = 0; col < matrix.length; col++) {
+            if ((col + 1) % 2 == 1) {
+                for (int row = 0; row < matrix.length; row++) {
+                    matrix[row][col] = startNumber;
+                    startNumber++;
+                }
+            } else {
+                for (int row = matrix.length - 1; row >= 0; row--) {
+                    matrix[row][col] = startNumber;
+                    startNumber++;
+                }
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
