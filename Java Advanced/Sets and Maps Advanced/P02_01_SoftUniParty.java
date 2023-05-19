d7e50518a25da19cbb909b4d55ef742c3ab17e1b
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-05-19 18:56:05 +0300

    Add files via upload

---
+++ Java Advanced/Sets and Maps Advanced/P02_01_SoftUniParty.java
@@ -0,0 +1,34 @@
+package JavaAdvanced.z_Practice.SetsMapsLab;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class P02_01_SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> guests = new TreeSet<>();
+        String command = scanner.nextLine();
+
+        while (!command.equals("PARTY")) {
+            String guest = command;
+            guests.add(guest);
+
+            command = scanner.nextLine();
+        }
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("END")) {
+            String guest = input;
+            guests.remove(guest);
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println(guests.size());
+        for (String guest : guests) {
+            System.out.println(guest);
+        }
+    }
+}
