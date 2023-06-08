3b261d739e8f7c438887ed1d53a4842b3bdaeefc
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-06-08 11:33:45 +0300

    Add files via upload

--- Java Advanced/Practice 2/P02_01_RallyRacing.java
+++ Java Advanced/Practice 2/P02_01_RallyRacing.java
@@ -1,89 +1,83 @@
-package JavaAdvanced.z_Practice.MultimensionalArraysExercise;
+package JavaAdvanced.Practice2;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class P02_01_RallyRacing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        int size = Integer.parseInt(scanner.nextLine());
         String carNumber = scanner.nextLine();
-        int km = 0;
-        String[][] trace = new String[n][n];
+        String[][] matrix = new String[size][size];
 
-        for (int row = 0; row < trace.length; row++) {
+        for (int row = 0; row < matrix.length; row++) {
             String[] arr = scanner.nextLine().split("\\s+");
-            trace[row] = arr;
+            matrix[row] = arr;
         }
 
         int currentRow = 0;
         int currentCol = 0;
-        List<Integer> tunnelsCoordinates = new ArrayList<>();
-        findTunnels(trace, tunnelsCoordinates);
         boolean hasFinished = false;
+        List<Integer> tunnelCoordinates = new ArrayList<>();
+        int km = 0;
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col].equals("T")) {
+                    tunnelCoordinates.add(row);
+                    tunnelCoordinates.add(col);
+                }
+            }
+        }
 
         String direction = scanner.nextLine();
+
         while (!direction.equals("End")) {
-            switch (direction) {
-                case "left":
-                    currentCol--;
-                    break;
-                case "right":
-                    currentCol++;
-                    break;
-                case "up":
-                    currentRow--;
-                    break;
-                case "down":
-                    currentRow++;
-                    break;
+            if (direction.equals("left")) {
+                currentCol--;
+            } else if (direction.equals("right")) {
+                currentCol++;
+            } else if (direction.equals("up")) {
+                currentRow--;
+            } else if (direction.equals("down")) {
+                currentRow++;
             }
 
-            String movedPlace = trace[currentRow][currentCol];
+            String position = matrix[currentRow][currentCol];
 
-            if (movedPlace.equals(".")) {
+            if (position.equals(".")) {
                 km += 10;
-            } else if (movedPlace.equals("F")) {
-                System.out.printf("Racing car %s finished the stage!%n", carNumber);
-                km += 10;
-                hasFinished = true;
-                break;
-            } else if (movedPlace.equals("T")) {
-                currentRow = tunnelsCoordinates.get(2);
-                currentCol = tunnelsCoordinates.get(3);
+            } else if (position.equals("T")) {
+                currentRow = tunnelCoordinates.get(2);
+                currentCol = tunnelCoordinates.get(3);
                 km += 30;
-                trace[tunnelsCoordinates.get(0)][tunnelsCoordinates.get(1)] = ".";
-                trace[tunnelsCoordinates.get(2)][tunnelsCoordinates.get(3)] = ".";
+                matrix[tunnelCoordinates.get(0)][tunnelCoordinates.get(1)] = ".";
+                matrix[tunnelCoordinates.get(2)][tunnelCoordinates.get(3)] = ".";
+            } else if (position.equals("F")) {
+                hasFinished = true;
+                km += 10;
+                break;
             }
 
             direction = scanner.nextLine();
         }
 
-        if (!hasFinished) {
+        if (hasFinished) {
+            System.out.printf("Racing car %s finished the stage!%n", carNumber);
+        } else {
             System.out.printf("Racing car %s DNF.%n", carNumber);
         }
 
         System.out.printf("Distance covered %d km.%n", km);
-        trace[currentRow][currentCol] = "C";
+        matrix[currentRow][currentCol] = "C";
 
-        for (int row = 0; row < trace.length; row++) {
-            for (int col = 0; col < trace[row].length; col++) {
-                System.out.print(trace[row][col]);
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col]);
             }
             System.out.println();
         }
     }
-
-    private static void findTunnels(String[][] trace, List<Integer> tunnelsCoordinates) {
-        for (int row = 0; row < trace.length; row++) {
-            for (int col = 0; col < trace[row].length; col++) {
-                String currentElement = trace[row][col];
-                if (currentElement.equals("T")) {
-                    tunnelsCoordinates.add(row);
-                    tunnelsCoordinates.add(col);
-                }
-            }
-        }
-    }
-
 }
