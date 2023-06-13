d9f9ffc6f49408ed90607c98bfa98c72c948cba2
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-06-13 15:27:47 +0300

    Add files via upload

---
+++ Java Advanced/Practice 2/P01_01_EnergyDrinks.java
@@ -0,0 +1,57 @@
+package JavaAdvanced.Practice2;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P01_01_EnergyDrinks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] caffeinesArray = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+        int[] drinksArray = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+
+        Stack<Integer> caffeinesStack = new Stack<>();
+        ArrayDeque<Integer> drinksQueue = new ArrayDeque<>();
+
+        for (int caffeine : caffeinesArray) {
+            caffeinesStack.push(caffeine);
+        }
+
+        for (int drink : drinksArray) {
+            drinksQueue.offer(drink);
+        }
+
+        int totalCaffeine = 0;
+
+        while (!caffeinesStack.isEmpty() && !drinksQueue.isEmpty()) {
+            int caffeine = caffeinesStack.peek();
+            int drink = drinksQueue.peek();
+            int currentCaffeine = caffeine * drink;
+
+            if (currentCaffeine + totalCaffeine <= 300) {
+                totalCaffeine += currentCaffeine;
+                caffeinesStack.pop();
+                drinksQueue.poll();
+            } else {
+                caffeinesStack.pop();
+                int value = drinksQueue.poll();
+                drinksQueue.offer(value);
+                totalCaffeine -= 30;
+                if (totalCaffeine < 0) {
+                    totalCaffeine = 0;
+                }
+            }
+        }
+
+        if (!drinksQueue.isEmpty()) {
+            System.out.print("Drinks left: ");
+            System.out.println(drinksQueue.toString().replace("[","").replace("]",""));
+        } else {
+            System.out.println("At least Stamat wasn't exceeding the maximum caffeine.");
+        }
+
+        System.out.printf("Stamat is going to sleep with %d mg caffeine.", totalCaffeine);
+    }
+}
