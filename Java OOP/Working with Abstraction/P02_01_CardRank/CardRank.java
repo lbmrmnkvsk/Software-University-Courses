95068b6e938696793484a2f24c85c76d4d1e2487
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-06-26 12:57:37 +0300

    Add files via upload

---
+++ Java OOP/Working with Abstraction/P02_01_CardRank/CardRank.java
@@ -0,0 +1,17 @@
+package JavaOOP.AbstractionExercise.P02_01_CardRank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
