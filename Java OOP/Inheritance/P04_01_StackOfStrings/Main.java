386ad31b704bbbaf05d01b25d740436395b8c3af
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-03 10:11:00 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/P04_01_StackOfStrings/Main.java
@@ -0,0 +1,9 @@
+package JavaOOP.InheritanceLab.P04_01_StackOfStrings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
