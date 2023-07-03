9194620e9f4996d1cff586c03385aff9e236346a
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-03 09:03:46 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/P01_01_SingleInheritance/Animal.java
@@ -0,0 +1,8 @@
+package JavaOOP.InheritanceLab.P01_01_SingleInheritance;
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ Java OOP/Inheritance/P01_01_SingleInheritance/Dog.java
@@ -0,0 +1,8 @@
+package JavaOOP.InheritanceLab.P01_01_SingleInheritance;
+
+public class Dog extends Animal{
+
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
