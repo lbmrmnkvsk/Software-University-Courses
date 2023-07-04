0f6b9365a2ee53b47de35d300208efd4801116a1
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-04 10:40:17 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/P04_01_NeedForSpeed/FamilyCar.java
@@ -0,0 +1,7 @@
+package JavaOOP.InheritanceExercise.P04_01_NeedForSpeed;
+
+public class FamilyCar extends  Car {
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
---
+++ Java OOP/Inheritance/P04_01_NeedForSpeed/Motorcycle.java
@@ -0,0 +1,7 @@
+package JavaOOP.InheritanceExercise.P04_01_NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
