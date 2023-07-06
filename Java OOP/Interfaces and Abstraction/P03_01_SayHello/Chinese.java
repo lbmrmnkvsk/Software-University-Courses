cf89c7040ac531656c91c86470908d027975a38c
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-06 15:41:24 +0300

    Add files via upload

---
+++ Java OOP/Interfaces and Abstraction/P03_01_SayHello/Chinese.java
@@ -0,0 +1,19 @@
+package JavaOOP.InterfacesLab.P03_01_SayHello;
+
+public class Chinese implements Person {
+    private String name;
+
+    public Chinese(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
---
+++ Java OOP/Interfaces and Abstraction/P03_01_SayHello/Person.java
@@ -0,0 +1,6 @@
+package JavaOOP.InterfacesLab.P03_01_SayHello;
+
+public interface Person {
+    String getName();
+    String sayHello();
+}
