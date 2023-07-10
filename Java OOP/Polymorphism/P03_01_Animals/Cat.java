640b07c34e6a56e1bee7e90721fcb6774961974b
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-10 10:20:54 +0300

    Add files via upload

---
+++ Java OOP/Polymorphism/P03_01_Animals/Cat.java
@@ -0,0 +1,12 @@
+package P03_01_Animals;
+
+public class Cat extends Animal {
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%nMEEOW", super.getName(), super.getFavouriteFood());
+    }
+}
