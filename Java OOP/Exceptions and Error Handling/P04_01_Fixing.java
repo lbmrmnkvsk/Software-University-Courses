5e5ec92b74ca5364c1ed7e68b35259175eed083b
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-18 10:58:01 +0300

    Add files via upload

---
+++ Java OOP/Exceptions and Error Handling/P04_01_Fixing.java
@@ -0,0 +1,24 @@
+package JavaOOP.Exceptions;
+
+import java.util.Scanner;
+
+public class P04_01_Fixing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] weekdays = new String[5];
+
+        weekdays[0] = "Monday";
+        weekdays[1] = "Tuesday";
+        weekdays[2] = "Wednesday";
+        weekdays[3] = "Thursday";
+        weekdays[4] = "Friday";
+
+        try {
+            for (int i = 0; i <= weekdays.length; i++) {
+                System.out.println(weekdays[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index");
+        }
+    }
+}
