749b4cd44ee9440e81e08f9479bfe60814cc7dd1
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-30 17:37:15 +0300

    Add files via upload

---
+++ Java OOP/Practice/P03_01_MagicGame/entities/booths/PrivateBooth.java
@@ -0,0 +1,7 @@
+package christmasPastryShop.entities.booths;
+
+public class PrivateBooth extends BaseBooth {
+    public PrivateBooth(int boothNumber, int capacity) {
+        super(boothNumber, capacity, 3.50);
+    }
+}
---
+++ Java OOP/Practice/P03_01_MagicGame/repositories/DelicacyRepositoryImpl.java
@@ -0,0 +1,31 @@
+package christmasPastryShop.repositories;
+
+import christmasPastryShop.entities.delicacies.interfaces.Delicacy;
+import christmasPastryShop.repositories.interfaces.DelicacyRepository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class DelicacyRepositoryImpl implements DelicacyRepository<Delicacy> {
+    private Collection<Delicacy> models;
+
+    public DelicacyRepositoryImpl() {
+        this.models = new ArrayList<>();
+    }
+
+    @Override
+    public Delicacy getByName(String name) {
+        return this.models.stream().filter(d -> d.getName().equals(name)).findFirst().orElse(null);
+    }
+
+    @Override
+    public Collection<Delicacy> getAll() {
+        return Collections.unmodifiableCollection(this.models);
+    }
+
+    @Override
+    public void add(Delicacy delicacy) {
+        this.models.add(delicacy);
+    }
+}
