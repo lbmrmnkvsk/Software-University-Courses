b6eb80ec4a01e98980448ffe244ff886abd2904f
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-07-31 10:10:28 +0300

    Add files via upload

---
+++ Java OOP/Practice/P01_01_GoldDigger/models/discoverer/BaseDiscoverer.java
@@ -0,0 +1,63 @@
+package goldDigger.models.discoverer;
+
+import goldDigger.common.ExceptionMessages;
+import goldDigger.models.museum.Museum;
+
+public abstract class BaseDiscoverer implements Discoverer {
+    private String name;
+    private double energy;
+    private Museum museum;
+
+    public BaseDiscoverer(String name, double energy) {
+        this.setName(name);
+        this.setEnergy(energy);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(ExceptionMessages.DISCOVERER_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        if (energy < 0) {
+            throw new IllegalArgumentException(ExceptionMessages.DISCOVERER_ENERGY_LESS_THAN_ZERO);
+        }
+        this.energy = energy;
+    }
+
+    @Override
+    public Museum getMuseum() {
+        return museum;
+    }
+
+    public void setMuseum(Museum museum) {
+        this.museum = museum;
+    }
+
+    @Override
+    public boolean canDig() {
+        return this.energy > 0;
+    }
+
+    @Override
+    public void dig() {
+        if (this.canDig()) {
+            this.energy -= 15;
+            if (this.energy < 0) {
+                this.energy = 0;
+            }
+        }
+    }
+}
