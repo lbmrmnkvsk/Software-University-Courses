58ae9ce65c920fad5e1a41f385dfc0113404b901
lbmrmnkvsk <122978156+lbmrmnkvsk@users.noreply.github.com>
2023-09-20 18:44:59 +0300

    Add files via upload

---
+++ Data Structures Fundamentals with Java/Trees Representation and Traversal/P02_01_CreateTree/implementations/Tree.java
@@ -0,0 +1,80 @@
+package implementations;
+
+import interfaces.AbstractTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree<E> implements AbstractTree<E> {
+
+    private E key;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+    public Tree(E element, Tree<E>... children) {
+        this.key = element;
+        this.children = new ArrayList<>();
+        for (Tree<E> child : children) {
+            this.children.add(child);
+            child.parent = this;
+        }
+    }
+
+    @Override
+    public void setParent(Tree<E> parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void addChild(Tree<E> child) {
+        this.children.add(child);
+    }
+
+    @Override
+    public Tree<E> getParent() {
+        return this.parent;
+    }
+
+    @Override
+    public E getKey() {
+        return this.key;
+    }
+
+    @Override
+    public String getAsString() {
+        return null;
+    }
+
+    @Override
+    public List<E> getLeafKeys() {
+        return null;
+    }
+
+    @Override
+    public List<E> getMiddleKeys() {
+        return null;
+    }
+
+    @Override
+    public Tree<E> getDeepestLeftmostNode() {
+        return null;
+    }
+
+    @Override
+    public List<E> getLongestPath() {
+        return null;
+    }
+
+    @Override
+    public List<List<E>> pathsWithGivenSum(int sum) {
+        return null;
+    }
+
+    @Override
+    public List<Tree<E>> subTreesWithGivenSum(int sum) {
+        return null;
+    }
+}
+
+
+
