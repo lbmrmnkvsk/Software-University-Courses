b47c4be55e43173a86546d6aac9fd94154d97dfa
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2023-11-11 18:21:45 +0200

    commit

---
+++ JS Front-End/Functions and Statements Exercise/P08_PerfectNumber.js
@@ -0,0 +1,22 @@
+function solve(n) {
+    if (!Number.isInteger(n) || n <= 0) {
+        return console.log("It's not so perfect.");
+    }
+
+    let sum = 0;
+    for (let i = 1; i < n; i++) {
+        if (n % i === 0) {
+            sum += i;
+        }
+    }
+
+    if (sum === n) {
+        console.log("We have a perfect number!");
+    } else {
+        console.log("It's not so perfect.");
+    }
+}
+
+solve(6);
+solve(28);
+solve(1236498);
