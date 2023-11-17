d3a43e497f5c29a65f6cbbb7aec09887ad8c2db3
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2023-11-17 14:55:59 +0200

    commit

---
+++ JS Front-End/Objects and Classes Lab/P03_ConvertToObject.js
@@ -0,0 +1,9 @@
+function solve(jsonString) {
+    let person = JSON.parse(jsonString);
+    for (let key in person) {
+        console.log(`${key}: ${person[key]}`);
+    }
+}
+
+solve('{"name": "George", "age": 40, "town": "Sofia"}');
+solve('{"name": "Peter", "age": 35, "town": "Plovdiv"}');
