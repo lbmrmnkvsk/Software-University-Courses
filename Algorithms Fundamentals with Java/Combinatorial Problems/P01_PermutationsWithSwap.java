eee9e6d208076712f2f33de272ad114642cb0511
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2023-12-25 16:56:20 +0200

    commit

---
+++ Algorithms Fundamentals with Java/Combinatorial Problems/P01_PermutationsWithSwap.java
@@ -0,0 +1,15 @@
+package L03_CombinatorialProblems;
+
+import java.util.Scanner;
+
+public class P01_PermutationsWithSwap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] elements = scanner.nextLine().split("\\s+");
+        permute(elements, 0);
+    }
+
+    public static void permute(String[] elements, int index) {
+
+    }
+}
