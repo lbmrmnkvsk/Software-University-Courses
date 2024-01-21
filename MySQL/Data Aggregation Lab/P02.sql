af0e148afd37ac77b8919ae16edb776a2639ce72
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-01-21 10:04:06 +0200

    commit

---
+++ MySQL/Data Aggregation Lab/P02.sql
@@ -0,0 +1,40 @@
+SELECT 
+    department_id, COUNT(id) AS 'Number of employees'
+FROM
+    employees
+GROUP BY department_id
+ORDER BY department_id , COUNT(id);
+
+SELECT 
+    department_id, ROUND(AVG(salary), 2) AS 'Average Salary'
+FROM
+    employees
+GROUP BY department_id
+ORDER BY department_id;
+
+SELECT 
+    department_id, ROUND(MIN(salary), 2) AS 'Min Salary'
+FROM
+    employees
+GROUP BY department_id
+HAVING `Min Salary` > 800
+ORDER BY department_id;
+
+SELECT 
+    COUNT(id)
+FROM
+    products
+WHERE
+    price > 8
+GROUP BY category_id
+HAVING category_id = 2;
+
+SELECT 
+    category_id,
+    ROUND(AVG(price), 2) AS 'Average Price',
+    ROUND(MIN(price), 2) AS 'Cheapest Product',
+    ROUND(MAX(price), 2) AS 'Most Expensive Product'
+FROM
+    products
+GROUP BY category_id
+ORDER BY category_id;
