61bbe3e87402940d7f16fab0a0593ed2dead3d05
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-01-23 17:27:05 +0200

    commit

---
+++ Algorithms Fundamentals with Java/Combinatorial Problems/P06_CombinationsWithRepetition.java
@@ -0,0 +1,28 @@
+package L03_CombinatorialProblems;
+
+import java.util.Scanner;
+
+public class P06_CombinationsWithRepetition {
+    private static String[] elements;
+    private static String[] combination;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        elements = scanner.nextLine().split("\\s+");
+        int k = Integer.parseInt(scanner.nextLine());
+        combination = new String[k];
+
+        generateCombinations(0, 0);
+    }
+
+    public static void generateCombinations(int index, int start) {
+        if (index == combination.length) {
+            System.out.println(String.join(" ", combination));
+        } else {
+            for (int i = start; i < elements.length; i++) {
+                combination[index] = elements[i];
+                generateCombinations(index + 1, i);
+            }
+        }
+    }
+}
