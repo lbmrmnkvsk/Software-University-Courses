e760e9e936f12b2ddaaee6a2496693f83922e7ee
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-02-03 17:09:56 +0200

    commit

---
+++ 09. Algorithms Fundamentals with Java/Searching, Sorting, and Greedy Algorithms/P03_QuickSort.java
@@ -0,0 +1,49 @@
+package L05_Searching_Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P03_QuickSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        quickSort(numbers, 0, numbers.length - 1);
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    private static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
+    }
+}
