cead46ca4b5d1fbb7051cb4af48afea2f9aae996
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-02-23 15:21:14 +0200

    commit

---
+++ 12. Spring Data/Custom ORM/P04/orm/DbContext.java
@@ -0,0 +1,12 @@
+package orm;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+
+public interface DbContext<E> {
+    boolean persist(E entity) throws IllegalAccessException, SQLException;
+    Iterable<E> find(Class<E> table) throws SQLException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException;
+    Iterable<E> find(Class<E> table, String where) throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+    E findFirst(Class<E> table) throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+    E findFirst(Class<E> table, String where) throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+}
