5fbca3dd9c682831cf56388e1832b44f78a12cc5
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-02-28 14:27:27 +0200

    commit

---
+++ 12. Spring Data/Spring Data Intro Lab/P01/org/example/l11_sd_intro/models/Account.java
@@ -0,0 +1,43 @@
+package org.example.l11_sd_intro.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal balance;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Account() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ 12. Spring Data/Spring Data Intro Lab/P01/org/example/l11_sd_intro/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package org.example.l11_sd_intro.repositories;
+
+import org.example.l11_sd_intro.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> getByUsername(String username);
+}
