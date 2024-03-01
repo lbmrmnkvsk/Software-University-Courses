896e3e98013a53dfc79062f98eea85bded09b72f
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-01 16:49:33 +0200

    commit

---
+++ 12. Spring Data/Spring Data Advanced Querying Lab/P05/com/example/advquerying/repositories/IngredientRepository.java
@@ -0,0 +1,15 @@
+package com.example.advquerying.repositories;
+
+import com.example.advquerying.entities.Ingredient;
+import com.example.advquerying.entities.Shampoo;
+import com.example.advquerying.entities.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    List<Ingredient> findByNameStartingWith(String prefix);
+    List<Ingredient> findByNameInOrderByPrice(List<String> names);
+}
