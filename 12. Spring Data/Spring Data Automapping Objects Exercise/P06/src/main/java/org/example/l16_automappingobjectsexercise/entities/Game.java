4df21792b5676c98da820dd0e59e407de7eb3e4b
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-03 19:04:47 +0200

    commit

---
+++ 12. Spring Data/Spring Data Automapping Objects Exercise/P06/src/main/java/org/example/l16_automappingobjectsexercise/entities/Game.java
@@ -0,0 +1,102 @@
+package org.example.l16_automappingobjectsexercise.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String trailer;
+    @Column(name = "image_thumbnail")
+    private String imageThumbnail;
+    private Double size;
+    private Double price;
+    @Column(length = 1000)
+    private String description;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+    @ManyToMany(mappedBy = "games")
+    private Set<User> users = new HashSet<>();
+
+    public Game() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+
+    public String getImageThumbnail() {
+        return imageThumbnail;
+    }
+
+    public void setImageThumbnail(String imageThumbnail) {
+        this.imageThumbnail = imageThumbnail;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
---
+++ 12. Spring Data/Spring Data Automapping Objects Exercise/P06/src/test/java/org/example/l16_automappingobjectsexercise/L16AutomappingObjectsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.l16_automappingobjectsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L16AutomappingObjectsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
