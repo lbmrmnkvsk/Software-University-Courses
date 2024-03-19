0faab2f3132433554e0ae6a3af6488a85e57dac3
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-19 21:46:43 +0200

    commit

---
+++ 13. Spring Fundamentals/Workshop 1/P04/src/main/java/org/example/pathfinder1/controllers/UserController.java
@@ -0,0 +1,32 @@
+package org.example.pathfinder1.controllers;
+
+import org.example.pathfinder1.entities.dtos.UserRegistrationDTO;
+import org.example.pathfinder1.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute("user")
+    public UserRegistrationDTO userRegistrationDTO() {
+        return new UserRegistrationDTO();
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        return "register";
+    }
+
+
+}
---
+++ 13. Spring Fundamentals/Workshop 1/P04/src/main/java/org/example/pathfinder1/entities/enums/UserRole.java
@@ -0,0 +1,5 @@
+package org.example.pathfinder1.entities.enums;
+
+public enum UserRole {
+    USER, MODERATOR, ADMIN
+}
