7260fe9f1138e43203bc27924fee12121e88895b
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-19 21:46:54 +0200

    commit

---
+++ 13. Spring Fundamentals/Workshop 1/P05/src/main/java/org/example/pathfinder1/services/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package org.example.pathfinder1.services.impl;
+
+import org.example.pathfinder1.entities.User;
+import org.example.pathfinder1.repositories.UserRepository;
+import org.example.pathfinder1.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return this.userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+}
