c5d09e27de9235eb3788f9562736b79ccd0d2bc3
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-20 21:21:17 +0200

    commit

---
+++ 13. Spring Fundamentals/Workshop 2/P01/src/main/java/org/example/pathfinder1/services/impl/CommentServiceImpl.java
@@ -0,0 +1,8 @@
+package org.example.pathfinder1.services.impl;
+
+import org.example.pathfinder1.services.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+}
