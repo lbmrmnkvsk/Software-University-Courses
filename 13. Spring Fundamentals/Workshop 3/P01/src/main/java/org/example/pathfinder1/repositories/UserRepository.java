45e14172825d2e4f63d38af2fd9c5c98df6b846c
lbmrmnkvsk <lyubomir.minkovski@gmail.com>
2024-03-21 15:41:14 +0200

    commit

---
+++ 13. Spring Fundamentals/Workshop 3/P01/src/main/java/org/example/pathfinder1/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.pathfinder1.repositories;
+
+import org.example.pathfinder1.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
